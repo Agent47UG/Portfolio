81e2415b3bc6d0772c26ff2b886dc87cb94bda6f
Agent47UG <ujwalghodeswar@gmail.com>
2023-09-25 06:47:59 +0530

    Finished the website

--- js/main.js
+++ js/main.js
@@ -83,3 +83,19 @@ const scrollActive = () =>{
 	})
 }
 window.addEventListener('scroll', scrollActive)
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__perfil, .about__image, .contact__mail', {origin: 'right'})
+
+sr.reveal('.home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data', {origin: 'left'})
+
+sr.reveal('.services__card, .projects__card', {interval : 100})
+
+sr.reveal('.footer__links, .footer__copy', {origin: 'bottom'})
