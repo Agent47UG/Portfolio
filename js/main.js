6e8c4565e7395725fe6820b1f6262b39379c76a4
Agent47UG <ujwalghodeswar@gmail.com>
2024-09-30 09:31:55 +0530

    added project

--- js/main.js
+++ js/main.js
@@ -92,11 +92,13 @@ const sr = ScrollReveal({
     delay: 400,
 })
 
-sr.reveal('.home__perfil, .about__image, .contact__mail', {origin: 'right'})
+sr.reveal('.home__perfil, .contact__mail', {origin: 'right'})
 
-sr.reveal('.home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data', {origin: 'left'})
+sr.reveal('.home__name, .home__info', {origin: 'left'})
 
-sr.reveal('.services__card, .projects__card', {interval : 100})
+sr.reveal('.about__container,.about__image, .about__info, .contact__social, .contact__data', {origin: 'left',duration:1500})
+
+sr.reveal('.services__card, .projects__card', {duration:1000})
 
 sr.reveal('.footer__links, .footer__copy', {origin: 'bottom'})
 
